05de0eb53a76ba2995198726052a90292768fb3d
Nico96432 <nicolo.caravello@isitgo.it>
2022-11-30 10:01:27 +0100

    main caravello giusto

--- Bogar_Caravello_Rizzi_VerificaGit/src/bogar_caravello_rizzi_verificagit/Bogar_Caravello_Rizzi_VerificaGit.java
+++ Bogar_Caravello_Rizzi_VerificaGit/src/bogar_caravello_rizzi_verificagit/Bogar_Caravello_Rizzi_VerificaGit.java
@@ -14,12 +14,33 @@ public class Bogar_Caravello_Rizzi_VerificaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String citta = " ";
-        float superficie = 0;
-        Aeroporto aeroporti[] = new Aeroporto[1];
-        aeroporti[0] = new Aeroporto("Roma", 100);
+        String citta=" ";
+        double superficie=0.0;
+        Aereo[] aerei = new Aereo[5];
+        Pilota[] piloti = new Pilota[5];
+
+        aerei[0]=new Aereo("A",100,"ALITALIA", "FFF");
+        aerei[1]=new Aereo("B",200,"ALIBABA", "CCC");
+        aerei[2]=new Aereo("C",300,"RIZZI", "GGG");
+        aerei[3]=new Aereo("D",100,"NON", "EEE");
+        aerei[4]=new Aereo("E",100,"ECCOCI", "HHH");
+
+        piloti[0]=new Pilota("AB","MARIO","BIANCHI","CAPO");
+        piloti[1]=new Pilota("CD","MARCO","NERO","COPILOTA");
+        piloti[2]=new Pilota("EF","MAURO","VERDE","CAPETTO");
+        piloti[3]=new Pilota("GH","MIMMO","BERARDI","CAPINO");
+        piloti[4]=new Pilota("IL","GRAZIANO","PELLE","VICE");
+    
         
-        System.out.println("L'aeroporto di " + citta + "ha una superificie di " + superficie );
+        System.out.println("L'aereoporto di "+citta+"ha una superficie di "+superficie);
+        
+        for(int i = 0; i< 4; i++)
+        {System.out.println("Gli aerei disponibili sono " + aerei[i]);
+        }
+        
+        for( int i = 0; i< 4; i++){
+            System.out.println("Ci lavorano i piloti"+piloti[i]);
+        }
     }
     
 }
